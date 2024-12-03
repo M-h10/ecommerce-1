af032ed0b60f0760ccf34ba68f4e15e7dfa801aa
M-h10 <m.haris3129@gmail.com>
2024-12-03 10:33:29 +0530

    uploaded

---
+++ frontend/src/Ecommerce_Assets/Assets/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../Frontend_Assets/logo.png'
+import cart_log from '../Frontend_Assets/cart_icon.png'
+export default function Navbar() {
+  return (
+    <div>
+        <nav >
+            ul
+        </nav>
+    </div>
+  )
+}
+
